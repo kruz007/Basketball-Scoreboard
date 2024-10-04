9941a513370f8429460bdceedb1d8ba0acabac6e
kruz <kruz.fa@gmail.com>
2024-10-04 18:31:19 +0100

    modified

--- app.js
+++ app.js
@@ -1,60 +1,55 @@
-let homeButton1 = document.getElementById('home-button-1')
-let homeButton2 = document.getElementById('home-button-2')
-let homeButton3 = document.getElementById('home-button-3')
-let homeScore = document.querySelector('.home-score')
-let guestScore = document.querySelector('.guest-score')
-let guestButton1 = document.getElementById('guest-button-1')
-let guestButton2 = document.getElementById('guest-button-2')
-let guestButton3 = document.getElementById('guest-button-3')
-let newGameButton = document.getElementById('new-game-button')
-let determinant = 0;
-let determinantB = 0;
+let homeButton1 = document.getElementById("home-button-1");
+let homeButton2 = document.getElementById("home-button-2");
+let homeButton3 = document.getElementById("home-button-3");
+let homeScore = document.querySelector(".home-score");
+let guestScore = document.querySelector(".guest-score");
+let guestButton1 = document.getElementById("guest-button-1");
+let guestButton2 = document.getElementById("guest-button-2");
+let guestButton3 = document.getElementById("guest-button-3");
+let newGameButton = document.getElementById("new-game-button");
+let determinantHome = 0;
+let determinantGuest = 0;
 
 let homePlus1 = () => {
-    determinant++;
-    homeScore.innerText = determinant
-}
-homeButton1.addEventListener('click', homePlus1)
+  determinantHome++;
+  homeScore.innerText = determinantHome;
+};
+homeButton1.addEventListener("click", homePlus1);
 
 let homePlus2 = () => {
-    determinant += 2
-    homeScore.innerText = determinant
-}
-homeButton2.addEventListener('click', homePlus2)
+  determinantHome += 2;
+  homeScore.innerText = determinantHome;
+};
+homeButton2.addEventListener("click", homePlus2);
 
 let homePlus3 = () => {
-    determinant += 3
-    homeScore.innerText = determinant
-}
-homeButton3.addEventListener('click', homePlus3)
-
-
-
-
+  determinantHome += 3;
+  homeScore.innerText = determinantHome;
+};
+homeButton3.addEventListener("click", homePlus3);
 
 let guestPlus1 = () => {
-    determinantB++;
-    guestScore.innerText = determinantB
-}
-guestButton1.addEventListener('click', guestPlus1)
+  determinantGuest++;
+  guestScore.innerText = determinantGuest;
+};
+guestButton1.addEventListener("click", guestPlus1);
 
 let guestPlus2 = () => {
-    determinantB += 2
-    guestScore.innerText = determinantB}
-guestButton2.addEventListener('click', guestPlus2)
+  determinantGuest += 2;
+  guestScore.innerText = determinantGuest;
+};
+guestButton2.addEventListener("click", guestPlus2);
 
 let guestPlus3 = () => {
-    determinantB += 3
-    guestScore.innerText = determinantB}
-guestButton3.addEventListener('click', guestPlus3)
-
-
-
+  determinantGuest += 3;
+  guestScore.innerText = determinantGuest;
+};
+guestButton3.addEventListener("click", guestPlus3);
 
 let reset = () => {
-    determinant = 0
-    determinantB = 0
-    homeScore.innerText = determinant
-    guestScore.innerText = determinantB
-}
-newGameButton.addEventListener('click', reset)
+  determinantHome = 0;
+  determinantGuest = 0;
+  homeScore.innerText = determinantHome;
+  guestScore.innerText = determinantGuest;
+};
+newGameButton.addEventListener("click", reset);
